46eee4d7f1a958ede08f97180508e0d70dc0516e
Eiren Rain <eirenliel@users.noreply.github.com>
2023-05-21 00:15:17 +0200

    It is what it is

--- server/src/main/java/com/jme3/system/NanoTimer.kt
+++ server/src/main/java/com/jme3/system/NanoTimer.kt
@@ -29,77 +29,46 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.jme3.system;
+package com.jme3.system
 
 /**
- * <code>NanoTimer</code> is a System.nanoTime implementation of
- * <code>Timer</code>. This is primarily useful for headless applications
+ * `NanoTimer` is a System.nanoTime implementation of
+ * `Timer`. This is primarily useful for headless applications
  * running on a server.
- * 
+ *
  * @author Matthew D. Hicks
  */
-public class NanoTimer extends Timer {
+open class NanoTimer : Timer() {
+    private var startTime = System.nanoTime()
+    private var previousTime: Long = 0
+    override var timePerFrame = 0f
+    override var frameRate = 0f
+    override var time: Long = 0
+	override val resolution = Companion.RESOLUTION
 
-	private static final long TIMER_RESOLUTION = 1000000000L;
-	private static final float INVERSE_TIMER_RESOLUTION = 1f / TIMER_RESOLUTION;
+    private val timeInternal: Long
+        /**
+         * Returns the time in seconds. The timer starts at 0.0 seconds.
+         *
+         * @return the current time in seconds
+         */
+		get() = System.nanoTime() - startTime
 
-	private long startTime;
-	private long previousTime;
-	private float tpf;
-	private float fps;
-	private long currentTime;
+    override fun update() {
+        time = timeInternal
+        timePerFrame = (time - previousTime) * (1.0f / resolution)
+        frameRate = 1.0f / timePerFrame
+        previousTime = time
+    }
 
-	public NanoTimer() {
-		startTime = System.nanoTime();
-	}
+    override fun reset() {
+        startTime = System.nanoTime()
+        time = timeInternal
+        previousTime = time
+    }
 
-	/**
-	 * Returns the time in seconds. The timer starts at 0.0 seconds.
-	 *
-	 * @return the current time in seconds
-	 */
-
-	protected long getTimeInternal() {
-		return System.nanoTime() - startTime;
-	}
-
-	@Override
-	public float getTimeInSeconds() {
-		return getTime() * INVERSE_TIMER_RESOLUTION;
-	}
-
-	@Override
-	public long getTime() {
-		return currentTime;
-	}
-
-	@Override
-	public long getResolution() {
-		return TIMER_RESOLUTION;
-	}
-
-	@Override
-	public float getFrameRate() {
-		return fps;
-	}
-
-	@Override
-	public float getTimePerFrame() {
-		return tpf;
-	}
-
-	@Override
-	public void update() {
-		currentTime = getTimeInternal();
-		tpf = (currentTime - previousTime) * (1.0f / TIMER_RESOLUTION);
-		fps = 1.0f / tpf;
-		previousTime = getTime();
-	}
-
-	@Override
-	public void reset() {
-		startTime = System.nanoTime();
-		currentTime = getTimeInternal();
-		previousTime = getTime();
-	}
+    companion object {
+		private val RESOLUTION = 1000000000L
+        private val INVERSE_TIMER_RESOLUTION = 1f / RESOLUTION
+    }
 }
--- server/src/main/java/com/jme3/system/Timer.kt
+++ server/src/main/java/com/jme3/system/Timer.kt
@@ -29,65 +29,65 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.jme3.system;
+package com.jme3.system
 
 /**
- * <code>Timer</code> is the base class for a high resolution timer. It is
+ * `Timer` is the base class for a high resolution timer. It is
  * created from getTimer("display system")
  *
  * @author Mark Powell
  * @version $Id: Timer.java,v 1.18 2007/03/09 10:19:34 rherlitz Exp $
  */
-public abstract class Timer {
+abstract class Timer {
+    /**
+     * Returns the current time in ticks. A tick is an arbitrary measure of time
+     * defined by the timer implementation. The number of ticks per second is
+     * given by `getResolution()`. The timer starts at 0 ticks.
+     *
+     * @return a long value representing the current time
+     */
+    abstract var time: Long
+		protected set
+    val timeInSeconds: Float
+        /**
+         * Returns the time in seconds. The timer starts at 0.0 seconds.
+         *
+         * @return the current time in seconds
+         */
+        get() = time / resolution.toFloat()
 
-	/**
-	 * Returns the current time in ticks. A tick is an arbitrary measure of time
-	 * defined by the timer implementation. The number of ticks per second is
-	 * given by <code>getResolution()</code>. The timer starts at 0 ticks.
-	 *
-	 * @return a long value representing the current time
-	 */
-	public abstract long getTime();
+    /**
+     * Returns the resolution of the timer.
+     *
+     * @return the number of timer ticks per second
+     */
+    abstract val resolution: Long
 
-	/**
-	 * Returns the time in seconds. The timer starts at 0.0 seconds.
-	 *
-	 * @return the current time in seconds
-	 */
-	public float getTimeInSeconds() {
-		return getTime() / (float) getResolution();
-	}
+    /**
+     * Returns the "calls per second". If this is called every frame, then it
+     * will return the "frames per second".
+     *
+     * @return The "calls per second".
+     */
+    abstract var frameRate: Float
+		protected set
 
-	/**
-	 * Returns the resolution of the timer.
-	 *
-	 * @return the number of timer ticks per second
-	 */
-	public abstract long getResolution();
+    /**
+     * Returns the time, in seconds, between the last call and the current one.
+     *
+     * @return Time between this call and the last one.
+     */
+    abstract var timePerFrame: Float
+		protected set
 
-	/**
-	 * Returns the "calls per second". If this is called every frame, then it
-	 * will return the "frames per second".
-	 *
-	 * @return The "calls per second".
-	 */
-	public abstract float getFrameRate();
+    /**
+     * `update` recalculates the frame rate based on the previous
+     * call to update. It is assumed that update is called each frame.
+     */
+    abstract fun update()
 
-	/**
-	 * Returns the time, in seconds, between the last call and the current one.
-	 *
-	 * @return Time between this call and the last one.
-	 */
-	public abstract float getTimePerFrame();
-
-	/**
-	 * <code>update</code> recalculates the frame rate based on the previous
-	 * call to update. It is assumed that update is called each frame.
-	 */
-	public abstract void update();
-
-	/**
-	 * Reset the timer to 0. Clear any tpf history.
-	 */
-	public abstract void reset();
+    /**
+     * Reset the timer to 0. Clear any tpf history.
+     */
+    abstract fun reset()
 }
